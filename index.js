a9626bb1e4d7aeb40aeb46c8091743a52232f926
AkiraAkaza <137365140+AkiraAkaza@users.noreply.github.com>
2024-02-02 17:18:24 +0700

    Create index.js
    index

---
+++ index.js
@@ -0,0 +1,26 @@
+const Eris = require("eris");
+require('./keep_alive.js')
+
+const bot = new Eris(process.env.token);
+
+bot.on("error", (err) => {
+  console.error(err);
+});
+
+bot.on("messageCreate", (msg) => {
+  if (msg.content.toLowerCase() === "*stream") {
+    console.log("Switching to STREAM status");
+    bot.editStatus("dnd", { name: "STREAMING", type: 1, url: "https://twitch.tv/akira_sachiko" });
+  } else if (msg.content.toLowerCase() === "*listening") {
+    console.log("Switching to LISTENING status");
+    bot.editStatus("dnd", { name: "LISTENING", type: 2 });
+  } else if (msg.content.toLowerCase() === "*playing") {
+    console.log("Switching to PLAYING status");
+    bot.editStatus("dnd", { name: "PLAYING", type: 0 });
+  } else if (msg.content.toLowerCase() === "*watching") {
+    console.log("Switching to WATCHING status");
+    bot.editStatus("dnd", { name: "WATCHING", type: 3 });
+  }
+});
+
+bot.connect();
